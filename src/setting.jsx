ac9048996ffb1b51db6cca99e80434d28a714440
psuknijarun <parinee.suknijarun@agoda.com>
2024-12-22 21:40:09 +0700

    Update setting.jsx

--- src/setting.jsx
+++ src/setting.jsx
@@ -1,2 +1,2 @@
-export const BASE_URL_SETTING = "http://127.0.0.1:8004";
-// export const BASE_URL_SETTING = "https://mockaibackend.vercel.app";
+//export const BASE_URL_SETTING = "http://127.0.0.1:8004";
+export const BASE_URL_SETTING = "https://mockaibackend.vercel.app";
